3be8098326cdd4b4dd593c3d35758f4903b83e71
yksoft1 <yksoft1@gmail.com>
2018-03-11 02:36:58 +0800

    Added build script for my MinGW environment.

---
+++ buildhx.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+export CFLAGS="-O3 -static-libgcc -flto -fno-use-linker-plugin "
+
+export CXXFLAGS="-O3 -static-libgcc -static-libstdc++ -flto -fno-use-linker-plugin  -DWINVER=0x0501 -D_WIN32_WINNT=0x0501"
+
+cd vs2015/sdl
+if test -f configure; then
+	configure --prefix=/mingw --disable-shared --enable-static
+else
+	bash autogen.sh
+	configure --prefix=/mingw --disable-shared --enable-static
+fi
+
+make -j4
+make install
+
+cd ../../
+if test -f configure; then
+	configure --disable-mt32 --disable-opengl --enable-hx-dos
+else
+	bash autogen.sh
+	configure --disable-mt32 --disable-opengl --enable-hx-dos
+fi
+make -j4
+cd src
+strip dosbox-x.exe
+mv dosbox-x.exe ../dosboxhx.exe
+cd ..
