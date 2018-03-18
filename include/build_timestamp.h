60ec466925010e54c479d2c6380efa50dcd78608
yksoft1 <yksoft1@gmail.com>
2018-03-18 13:41:45 +0800

    Fix build timestamp.

--- include/build_timestamp.h
+++ include/build_timestamp.h
@@ -1,7 +1,2 @@
 /*auto-generated*/
-<<<<<<< HEAD
-#define UPDATED_STR "Mar 16, 2018 7:53:32pm"
-=======
-#define UPDATED_STR "Mar 16, 2018 7:53:32pm"
->>>>>>> upstream/master
-#define COPYRIGHT_END_YEAR "2018"
+#define UPDATED_STR "Mar 18, 2018"
